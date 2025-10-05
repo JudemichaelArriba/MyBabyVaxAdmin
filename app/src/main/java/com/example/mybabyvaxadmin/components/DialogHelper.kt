303b19b21ad162b341082859b936c1bae03c8906
JudeMichaelArriba <arribajudemichael@gmail.com>
2025-10-05 17:00:34 +0800

    change color dialog button

--- app/src/main/java/com/example/mybabyvaxadmin/components/DialogHelper.kt
+++ app/src/main/java/com/example/mybabyvaxadmin/components/DialogHelper.kt
@@ -4,27 +4,24 @@ import android.content.Context
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.LinearLayout
+import androidx.core.content.ContextCompat
 import com.developer.kalert.KAlertDialog
-import com.developer.kalert.R
-
-import kotlin.apply
-import kotlin.collections.forEach
-import kotlin.let
+import com.example.mybabyvaxadmin.R
 
 class DialogHelper {
 
     companion object {
 
         private fun enlargeButtons(dialog: KAlertDialog) {
+
             val confirmButton =
-                dialog.findViewById<Button>(R.id.custom_confirm_button)
-            val cancelButton = dialog.findViewById<Button>(R.id.cancel_button)
+                dialog.findViewById<Button>(com.developer.kalert.R.id.custom_confirm_button)
+            val cancelButton =
+                dialog.findViewById<Button>(com.developer.kalert.R.id.cancel_button)
 
             listOf(confirmButton, cancelButton).forEach { button ->
                 button?.let {
                     val params = it.layoutParams
-
-
                     if (params is ViewGroup.LayoutParams) {
                         val newParams = (params as? LinearLayout.LayoutParams)
                         newParams?.apply {
@@ -35,6 +32,12 @@ class DialogHelper {
                         it.layoutParams = newParams
                     }
 
+
+                    it.backgroundTintList = null
+                    it.background = null
+                    it.setBackgroundColor(ContextCompat.getColor(dialog.context, R.color.mainColor))
+
+                    it.setTextColor(ContextCompat.getColor(dialog.context, android.R.color.white))
                     it.textSize = 18f
                 }
             }
